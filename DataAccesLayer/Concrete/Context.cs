f50c2079dff49f6a05c39f8b00d91ed745c40270
Asus <Asus@DESKTOP-D6N7I1U>
2023-07-24 00:08:56 +0300

    Add project files.

---
+++ DataAccesLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Concrete
+{
+   public class Context:DbContext
+    {
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-D6N7I1U;database=Fizyoterapi;İntegrated security=true;");
+        }
+
+    }
+}
